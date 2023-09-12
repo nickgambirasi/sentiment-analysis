297be773eac4bb2d6ece625f3bd950a91076a1fa
Nick Gambirasi <nick.a.gambirasi@gmail.com>
2023-09-12 15:30:26 -0600

    new compare functionality addition + new styles for comparison + new buttons for comparisons and new airline selection (#15)
    Co-authored-by: Nick <nickgambirasi@gmail.com>

--- app/app.py
+++ app/app.py
@@ -30,6 +30,10 @@ APP_ROOT = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 
 global pred_data
+global airlines
+
+pred_data = None
+airlines = []
 
 
 @app.route("/")
@@ -40,11 +44,14 @@ def redirect():
 @app.route("/select-airline")
 def select_airline():
     global pred_data
-    pred_data = collect_and_predict()
-    airlines = []
-    with open(os.path.join(APP_ROOT, os.pardir, "data/airlines.txt")) as f:
-        for line in f.readlines():
-            airlines.append(line.strip().replace(" ", "_"))
+    global airlines
+
+    if pred_data is None:
+        pred_data = collect_and_predict()
+    if not airlines:
+        with open(os.path.join(APP_ROOT, os.pardir, "data/airlines.txt")) as f:
+            for line in f.readlines():
+                airlines.append(line.strip().replace(" ", "_"))
     context = {"airlines": airlines}
     return render_template("homepage.html", **context)
 
@@ -56,10 +63,40 @@ def results(airline_name: str):
     from the initial webpage.
     """
     global pred_data
-    # collects all prediction data
-    airline_name = airline_name.replace("_", " ")
-    airline_data = pred_data.query("airline==@airline_name")
-    return render_template("results.html", graphJSON=plot_data(data_df=airline_data))
+    global airlines
+
+    airlines_copy = airlines.copy()
+    try:
+        airlines_copy.remove(airline_name)
+    except ValueError as e:
+        pass
+    airline_data = pred_data.query("airline==@airline_name.replace('_', ' ')")
+    context = {
+        "graphJSON": plot_data(data_df=airline_data),
+        "airline": airline_name,
+        "unselected_airlines": airlines_copy,
+    }
+    return render_template("results.html", **context)
+
+
+@app.route("/analysis/compare/<airline1_name>+<airline2_name>")
+def compare(airline1_name: str, airline2_name: str):
+    global pred_data
+
+    # airline1_name = airline1_name.replace("_", " ")
+    # airline2_name = airline2_name.replace("_", " ")
+
+    airline1_data = pred_data.query("airline==@airline1_name.replace('_', ' ')")
+    airline2_data = pred_data.query("airline==@airline2_name.replace('_', ' ')")
+
+    context = {
+        "airline1_name": airline1_name,
+        "airline2_name": airline2_name,
+        "airline1_graphJSON": plot_data(data_df=airline1_data),
+        "airline2_graphJSON": plot_data(data_df=airline2_data),
+    }
+
+    return render_template("compare.html", **context)
 
 
 if __name__ == "__main__":
